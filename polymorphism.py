b2c37154e8cdb7acc56d2f2319270accf30507ed
Anshika Gupta <anshiisgupta@gmail.com>
2020-06-25 20:29:34 +0545

    inheritance and polymorphism

---
+++ polymorphism.py
@@ -0,0 +1,21 @@
+﻿
+class Dog():
+
+	def __init__(self, name):
+		self.name = name
+
+	def speak(self):
+		return self.name + " says woof!!!"
+
+class Cat():
+
+	def __init__(self, name):
+		self.name = name
+
+	def speak(self):
+		return self.name + " says meow!!!"
+
+tommy = Dog("Tommy")
+kitty = Cat("Kitty")
+print(tommy.speak())
+print(kitty.speak())
