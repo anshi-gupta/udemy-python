fba972f069115db502f45678900c6b9c9b6036f3
Anshika Gupta <anshiisgupta@gmail.com>
2020-06-27 16:24:54 +0545

    space

--- ClassObjAttribute.py
+++ ClassObjAttribute.py
@@ -1,13 +1,13 @@
 
 class Circle():
-	pi = 3.14
+    pi = 3.14
 
-	def __init__(self, radius):
-		self.radius = radius
-		self.area = radius * radius * self.pi           # Class objects can be called using self as well as the name of the class
+    def __init__(self, radius):
+        self.radius = radius
+        self.area = radius * radius * self.pi           # Class objects can be called using self as well as the name of the class
 
-	def circumference(self):
-		return self.radius * Circle.pi * 2
+    def circumference(self):
+        return self.radius * Circle.pi * 2
 
 my_circle = Circle(5)
 print(my_circle.circumference())
