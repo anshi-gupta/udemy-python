453f6210b380dc0ecf810afce4ec15c1b3c1b06f
Anshika Gupta <anshiisgupta@gmail.com>
2020-07-10 19:06:50 +0545

    returning a function within another function

--- decorator.py
+++ decorator.py
@@ -1,6 +1,23 @@
-﻿def hello(name = "Anshi"):
+﻿def hello(name = 'Anshi'):
   print('The hello() function has been executed')
+  
   def greet():
     return '\t This is the greet () func inside hello'
-  print(greet())
-hello()
+  
+  def welcome():
+    return '\t This is welcome() inside hello'
+  
+  print('I am going to return a function!!')
+
+  if name == 'Anshi':
+    return greet
+    
+  else:
+    return welcome
+    
+
+my_new_func = hello('Anshi')
+print(my_new_func())
+
+my_next_func = hello('Avi')
+print(my_next_func())
