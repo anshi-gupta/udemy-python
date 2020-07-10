a606f3f2e5abdcc968c160d7bad45b0289acf81f
Anshika Gupta <anshiisgupta@gmail.com>
2020-07-10 22:10:52 +0545

    using next and iter function and yeild keyword

---
+++ fibonacii.py
@@ -0,0 +1,11 @@
+﻿
+#  the yield statement is used to define generators, replacing the return of a function to provide a result to its caller without destroying local variables.
+#   Unlike a function, where on each call it starts with new set of variables, a generator will resume the execution where it was left off
+def gen_fibon(n):
+  a = 1
+  b = 1
+  for i in range(n):
+    yield a
+    a, b = b, a+b
+for num in gen_fibon(10):
+  print(num)
---
+++ next_and_iter.py
@@ -0,0 +1,32 @@
+﻿# next function
+def sim_gen():
+  for x in range(3):
+    yield x
+for num in sim_gen():
+  print(num)
+
+g = sim_gen()
+print("Using next function")
+print("Gives the first number")
+print(next(g))
+print("Gives the next number")
+print(next(g))
+print("Gives the next number")
+print(next(g))
+
+# iter function
+# str object is not an iterator 
+# turn the string into generator using iter
+print("\n")
+print("An example of Iter function")
+s = "hello"
+for letter in s:
+  print(letter)
+
+print("After using iter function")
+s_iter = iter(s)
+print(next(s_iter))
+print(next(s_iter))
+print(next(s_iter))
+print(next(s_iter))
+print(next(s_iter))
