6929d4f7f6a24a4e02ee8dcb5e665566ca6b122d
Anshika Gupta <anshiisgupta@gmail.com>
2020-07-12 21:54:58 +0545

    word count

---
+++ harrypotter.py
@@ -0,0 +1,13 @@
+﻿
+f = open("HarryPotter.txt", "r+")
+word_count = {}
+for word in f.read().split():
+  if word not in word_count:
+    word_count[word] = 1
+
+  else:
+    word_count[word] +=1
+
+for k,v in word_count.items():
+  print(k, v)
+f.close()
