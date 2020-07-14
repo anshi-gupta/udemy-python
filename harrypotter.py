447632491c8cf5a9ca4c487f7c9d1c6b51ca5f2c
Anshika Gupta <anshiisgupta@gmail.com>
2020-07-14 22:34:02 +0545

    removed unwanted apostrophe

--- harrypotter.py
+++ harrypotter.py
@@ -1,19 +1,31 @@
-﻿
-f = open("HarryPotter.txt", "r+")
+﻿f = open("HarryPotter.txt", "r+")
 word_count = {}
 
-punc = '.,!_-:;"?/(){}[]'
+punc = '''.,!_-:;"?/(){}[]'''
 no_punc = ""
+z = ""
+
 for char in f.read():
   if char not in punc:
     no_punc = no_punc + char
 
 for word in no_punc.split():
-  if word not in word_count:
-    word_count[word] = 1
+    if word[0] == "'":
+      first = word[1:]
+      z = z + first + " "
+    elif word[-1] == "'":
+      last = word[:-1]
+      z = z + last + " "
+    else:
+      no_apostrophe = word
+      z = z + no_apostrophe + " "
+
+for word_new in z.split():
+  if word_new not in word_count:
+    word_count[word_new] = 1
 
   else:
-    word_count[word] +=1
+    word_count[word_new] +=1
 
 for k,v in word_count.items():
   print(k, v)
