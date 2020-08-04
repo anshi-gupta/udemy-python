c9f902631688c904f5d1c3efb0b422689ffd0b22
Anshika Gupta <anshiisgupta@gmail.com>
2020-08-04 17:22:17 +0545

    leap year problem

---
+++ largest_element_of_array.py
@@ -0,0 +1,16 @@
+﻿# Program to find the largest element of an array of size n
+
+def largest(arr, n):
+  # initialize maximum element
+  max = arr[0]
+
+  for i in range(1, n):
+    if arr[i] > max:
+      max = arr[i]
+  return max
+
+arr = [2, 10, 100, 45, 32]
+n = len(arr)
+result = largest(arr, n)
+print("The array is ", arr)
+print("The largest element of given array is ", result)
