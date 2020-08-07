b760d425573ef3df764651b28e67a98b9721d35c
Anshika Gupta <anshiisgupta@gmail.com>
2020-08-07 13:17:53 +0545

    TEXT WRAP

---
+++ TextWrap.py
@@ -0,0 +1,7 @@
+﻿import textwrap
+string = input("The string: ")
+max_width = int(input("Provide width: "))
+def wrapped_string(string, max_width):
+  return textwrap.fill(string, max_width)
+result = wrapped_string(string, max_width)
+print(result)
