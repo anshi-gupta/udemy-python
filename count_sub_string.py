d745d0bc3df9c475288e7c6b6048a6ed6119956f
Anshika Gupta <anshiisgupta@gmail.com>
2020-08-07 12:58:34 +0545

    sunstring count

---
+++ count_sub_string.py
@@ -0,0 +1,10 @@
+﻿# program to count the substring inside a string
+
+s = input("The string is ").strip()
+sb = input("The substring is ").strip()
+results = 0
+sub_len = len(sb)
+for i in range(len(s)):
+    if s[i:i+sub_len] == sb:
+        results += 1
+print ("Number of substrings present in the string: ", results)
