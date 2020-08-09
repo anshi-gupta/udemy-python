43549eb7e69cd93c6971c558ed4949b7fa78236b
Anshika Gupta <anshiisgupta@gmail.com>
2020-08-09 22:30:38 +0545

    Some examples of string formatting

--- string_formatting.py
+++ string_formatting.py
@@ -1,7 +1,9 @@
 ﻿
-n = int(input())
+n = int(input("Enter a number: "))
 binary = "{0:b}"
 l = len(binary.format(n))
+print("Values of given decimal number from 1 to {} in octal, hexadecimal and binary".format(n))
+print("Dec".center(l), "Oct".center(l), "Hex".center(l), "Bin".center(l))
 
 for i in range(1, n+1):
     dec = "{0:d}"
