ff8fbefb0612023a0e698426b5aface4314147de
Anshika Gupta <anshiisgupta@gmail.com>
2020-08-09 22:22:44 +0545

    string formatting

--- string_formatting.py
+++ string_formatting.py
@@ -1,14 +1,14 @@
-
+﻿
 n = int(input())
 binary = "{0:b}"
 l = len(binary.format(n))
 
 for i in range(1, n+1):
     dec = "{0:d}"
-    oct = "{0:o}"
-    hex = "{0:X}"
+    octal = "{0:o}"
+    hexa = "{0:X}"
     binary = "{0:b}"
-    print(dec.format(i).rjust(l), oct.format(i).rjust(l), hex.format(i).rjust(l), binary.format(i).rjust(l))
+    print(dec.format(i).rjust(l), octal.format(i).rjust(l), hexa.format(i).rjust(l), binary.format(i).rjust(l))
 
 
 
