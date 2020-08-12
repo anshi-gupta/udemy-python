f98a355d5baf2c8cb992314db64035b0c211377f
Anshika Gupta <anshiisgupta@gmail.com>
2020-08-12 10:20:01 +0545

    capitaizing the word

---
+++ capitalize.py
@@ -0,0 +1,9 @@
+﻿# Capitalizing first letter of each word 
+name = input("Give a name: ")
+name_list = name.split()
+name_len = len(name_list)
+new_name_list = []
+for i in range(0, name_len):
+    cap = name_list[i].capitalize()
+    new_name_list.append(cap)
+print(" ".join(new_name_list))
