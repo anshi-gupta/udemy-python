1e8edbfe53d5e2dd71674eacbda35c44ec5bb597
Anshika Gupta <anshiisgupta@gmail.com>
2020-08-12 14:37:08 +0545

    word starting from a digit is not capitalized

--- capitalize.py
+++ capitalize.py
@@ -2,8 +2,16 @@
 name = input("Give a name: ")
 name_list = name.split()
 name_len = len(name_list)
-new_name_list = []
-for i in range(0, name_len):
-    cap = name_list[i].capitalize()
-    new_name_list.append(cap)
-print(" ".join(new_name_list))
+result = name.title()
+
+##new_name_list = []
+##for i in range(0, name_len):
+##    cap = name_list[i].capitalize()
+##    new_name_list.append(cap)
+##print(" ".join(new_name_list))
+
+for i in range(name_len):
+    if name_list[i][0].isdigit() == True:
+        alnum_str = name_list[i]
+        result = result.replace(alnum_str.title(), alnum_str)
+print(result)
