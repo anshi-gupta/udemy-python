54039f27aa9df8331781f0e408ba6f07227d1b07
Anshika Gupta <anshiisgupta@gmail.com>
2020-08-29 13:46:09 +0545

    HackerRank Problems

---
+++ FindAngle.py
@@ -0,0 +1,14 @@
+﻿##  Given a right angled triangle ABC.
+##  M is the mid-point of hypothenus,AC.
+##  INPUT - The other two sides of the triangle ABC, AB and BC.
+##  OUTPUT - Angle MBC
+
+import math
+AB = int(input())
+BC = int(input())
+AC = math.hypot(AB, BC)
+MC = AC / 2
+MBC = math.acos(0.5 * BC/MC)
+result = math.degrees(MBC)
+print(round(result), end = u'\N{DEGREE SIGN}')
+
---
+++ TriangleQuest.py
@@ -0,0 +1,20 @@
+﻿## TRIANGLE QUEST PROBLEM 1
+## INPUT:  5
+## OUTPUT: 1
+##         22
+##         333
+##         4444
+
+for i in range(1,int(input())):  #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print( (i * (10**i-1)) // 9 )
+
+## TRIANGLE QUEST PROBLEM 2
+## INPUT:  5
+## OUTPUT: 1
+##         121
+##         12321
+##         1234321
+##         123454321
+
+for i in range(1,int(input())+1): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print((10**i//9)**2)
