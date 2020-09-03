8207e89ef3a550965c0b6066c1d096ab27955224
Anshika Gupta <anshiisgupta@gmail.com>
2020-09-03 23:24:53 +0545

    4 line code to calculate average of marks using namedtuple

---
+++ CollectionsNamedTuple.py
@@ -0,0 +1,20 @@
+﻿## Dr. John Wesley has a spreadsheet containing a list of student's ID, MARKS, CLASS and NAME.
+## Your task is to help Dr. Wesley calculate the average marks of the students.
+
+## INPUT FORMAT
+## The first line contains an integer N, the total number of students.
+## The second line contains the names of the columns in any order.
+## The next  lines contains the ID, MARKS, CLASS and NAME under their respective column names.
+
+## OUTPUT FORMAT
+## Print the average marks of the list corrected to 2 decimal places.
+
+from collections import namedtuple
+N = int(input())
+StudentInfo = namedtuple('StudentInfo', ' '.join(split for split in (input().strip().split())))
+print("%.2f" % (sum(int(StudentInfo._make(input().strip().split()).MARKS) for _ in range(N))/N))
+
+
+
+
+
